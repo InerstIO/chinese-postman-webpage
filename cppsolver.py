e47523b77f09b18b39a56ff1265d02fa45977e69
fen9yu <fengyu@sjtu.edu.cn>
2017-12-03 16:49:01 -0800

    The animation seems good now.

--- cppsolver.py
+++ cppsolver.py
@@ -4,19 +4,31 @@ from chinesepostman import eularian, network
 import json
 
 def solver(degree = 0, weight = None, start = None):
-    """ Make it so. """
     edges = None
     edges = csvparser.parse('graph/graph.csv')
+    oriedges = edges
     oriedges4json = [{'data':{'source':str(edge[0]),'target':str(edge[1]),'weight':str(edge[2])}} for \
         edge in edges]
+    nodes = set(sum([[edge[0],edge[1]] for edge in edges],[]))
+    nodes4json = [{'data':{'id':str(n)}} for n in nodes]
     edges = preproc.check_degree(edges, degree)
     edges = preproc.check_weight(edges, weight)
+    deledges = [edge for edge in oriedges if edge not in edges]
     if not edges:
-        return ['There is no subgraph that meets the specification.']
+        return ['There is no subgraph that meets the specification.', \
+            json.dumps(nodes4json).replace('"data"','data').replace('"id"','id'), \
+            json.dumps(oriedges4json).replace('"data"','data').replace('"source"','source')\
+            .replace('"target"','target').replace('"weight"','weight').replace('"classes"','classes'),
+            [],json.dumps(nodeinpath),json.dumps(deledges)]
     convert, skip = preproc.check_index(edges)
+    nodeinpath = convert
     original_graph = network.Graph(edges)
     if not preproc.check_connected(original_graph):
-        return ['The graph is not connected (after meeting the specification).']
+        return ['The graph is not connected (after meeting the specification).',  \
+            json.dumps(nodes4json).replace('"data"','data').replace('"id"','id'), \
+            json.dumps(oriedges4json).replace('"data"','data').replace('"source"','source')\
+            .replace('"target"','target').replace('"weight"','weight').replace('"classes"','classes'),
+            [],json.dumps(nodeinpath),json.dumps(deledges)]
 
     #print('{} edges'.format(len(original_graph)))
     if not original_graph.is_eularian:
@@ -30,18 +42,26 @@ def solver(degree = 0, weight = None, start = None):
 
     if start != None and skip:
         if start not in convert:
-            return ['The start node specified does not meet other requirements, and is deleted.']
+            return ['The start node specified does not meet other requirements.', \
+            json.dumps(nodes4json).replace('"data"','data').replace('"id"','id'), \
+            json.dumps(oriedges4json).replace('"data"','data').replace('"source"','source')\
+            .replace('"target"','target').replace('"weight"','weight').replace('"classes"','classes'),
+            [],json.dumps(nodeinpath),json.dumps(deledges)]
         start = convert.index(start) + 1
     #print('Attempting to solve Eularian Circuit...')
     route, attempts = eularian.eularian_path(graph, start=start)
     if not route:
         #print('\tGave up after {} attempts.'.format(attempts))
-        return ['Failed to find a route after '+ str(attempts) + ' attempts.']
+        return ['Failed to find a route after '+ str(attempts) + ' attempts.', \
+            json.dumps(nodes4json).replace('"data"','data').replace('"id"','id'), \
+            json.dumps(oriedges4json).replace('"data"','data').replace('"source"','source')\
+            .replace('"target"','target').replace('"weight"','weight').replace('"classes"','classes'),
+            [],json.dumps(nodeinpath),json.dumps(deledges)]
     else:
         #print('\tSolved in {} attempts'.format(attempts, route))
         #print('Solution: ({} edges)'.format(len(route) - 1))
         #print('\t{}'.format(route))
-        nodes4json = [{'data':{'id':str(n)}} for n in convert]
+        #nodes4json = [{'data':{'id':str(n)}} for n in convert]
         if skip:
             for i, node in enumerate(route):
                 route[i] = convert[node-1]
@@ -52,4 +72,4 @@ def solver(degree = 0, weight = None, start = None):
             json.dumps(nodes4json).replace('"data"','data').replace('"id"','id'), \
             json.dumps(oriedges4json).replace('"data"','data').replace('"source"','source')\
             .replace('"target"','target').replace('"weight"','weight').replace('"classes"','classes'),
-            json.dumps(path4json)]
+            json.dumps(path4json),json.dumps(nodeinpath),json.dumps(deledges)]
